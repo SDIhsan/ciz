796fa41b1bd8b1c6e3736aef851ebe213f476266
anggito-m <anggitomuhammadamien@mail.ugm.ac.id>
2024-04-05 07:53:04 +0700

    add login form

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,6 +5,6 @@ class Login extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('login');
 	}
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./application/views/*.php",
+		"./application/views/**/*.php",
+		"./application/views/**/**/*.php",
+		"./application/views/**/**/**/*.php",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
